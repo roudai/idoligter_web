5f5b487ad959e8cca9899cef132640c17fe7f605
roudai <me@roudai.net>
2022-02-14 02:08:43 +0900

    インポート機能追加

--- idolitter/main/admin.py
+++ idolitter/main/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportMixin
 from .models import Group, Idol
 
-admin.site.register(Group)
-admin.site.register(Idol)
+class groupModelAdmin(ImportMixin, admin.ModelAdmin):
+  class groupResource(resources.ModelResource):
+    class Meta:
+      model = Group
+      fields = ('id', 'name', 'twitterUsername', 'twitterId', 'twitterName', 'twitterDescription')
+  resource_class = groupResource
+
+  list_display = ('name', 'twitterUsername', 'twitterId', 'twitterName', 'twitterDescription')
+  fields = ('name', 'twitterUsername')
+
+class idolModelAdmin(ImportMixin, admin.ModelAdmin):
+  list_display = ('name', 'twitterUsername', 'twitterId', 'twitterName', 'twitterDescription')
+  fields = ('group', 'name', 'twitterUsername')
+  filter_horizontal = ('group',)
+
+admin.site.register(Group, groupModelAdmin)
+admin.site.register(Idol, idolModelAdmin)
