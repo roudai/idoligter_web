ffe91c201d931c42739dd8e40134da9ee4805f2f
roudai <me@roudai.net>
2022-02-14 01:50:13 +0900

    Group, Idolモデルの追加

--- idolitter/main/admin.py
+++ idolitter/main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Group, Idol
 
-# Register your models here.
+admin.site.register(Group)
+admin.site.register(Idol)
--- idolitter/main/models.py
+++ idolitter/main/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Group(models.Model):
+  """アイドルグループモデル"""
+  class Meta:
+    db_table = 'group'
+  
+  name = models.CharField(verbose_name='グループ名', max_length=255, unique=True)
+  twitterUsername = models.CharField(verbose_name='Twitter Username', max_length=15)
+  twitterId = models.CharField(verbose_name='Twitter ID', max_length=20, null=True)
+  twitterName = models.CharField(verbose_name='Twitter Name', max_length=50, null=True)
+  twitterDescription = models.TextField(verbose_name='Twitter Description', null=True)
+
+  def __str__(self):
+    return self.name
+
+class Idol(models.Model):
+  """アイドルモデル"""
+  class Meta:
+    db_table = 'idol'
+
+  group = models.ManyToManyField(Group, verbose_name='グループ名')
+  name = models.CharField(verbose_name='名前', max_length=255, unique=True)
+  twitterUsername = models.CharField(verbose_name='Twitter Username', max_length=15)
+  twitterId = models.CharField(verbose_name='Twitter ID', max_length=20, null=True)
+  twitterName = models.CharField(verbose_name='Twitter Name', max_length=50, null=True)
+  twitterDescription = models.TextField(verbose_name='Twitter Description', null=True)
+
+  def __str__(self):
+    return self.name
